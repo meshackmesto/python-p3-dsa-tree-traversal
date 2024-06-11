8985d38421f024fc3a7d654017008e05055295d2
meshackmesto <meshackorina93@gmail.com>
2024-06-11 12:07:16 +0300

    done

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,13 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  def get_element_by_id(self, id,):
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      node= nodes_to_visit.pop()
+      if node['id'] == id:
+        return node
+
+      nodes_to_visit = nodes_to_visit + node['children']
+    return None
